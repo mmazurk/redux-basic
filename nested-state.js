4c187aa42333fa9e5810f22896192d7af2086ee5
markmazurk <markmazurk@gmail.com>
2024-04-29 11:32:24 -0700

    create nested state and update with immer

---
+++ nested-state.js
@@ -0,0 +1,49 @@
+const redux = require("redux");
+const produce = require("immer").produce;
+
+const createStore = redux.legacy_createStore;
+
+// action type
+const STREET_UPDATED = "STREET_UPDATED";
+
+// action creator
+function updateStreet(street) {
+  return {
+    type: STREET_UPDATED,
+    payload: street,
+  };
+}
+
+// initial state
+const initialState = {
+  name: "Flakrus",
+  address: {
+    street: "7777 99th Ave SE",
+    city: "Seattle",
+    state: "Washington",
+  },
+};
+
+// reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STREET_UPDATED:
+      const nextState = produce(state, (draft) => {
+        draft.address.street = action.payload;
+      });
+      return nextState;
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const unsubscribe = store.subscribe(() => {
+  console.log("State is", store.getState());
+});
+
+store.dispatch(updateStreet("1111 New Lane"));
+store.dispatch(updateStreet("2222 Whatever Road"));
+unsubscribe();
